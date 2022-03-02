27dc6af355978f29752c0e49be02c216e0de36db
ViniCSena <cardosovinicius32@gmail.com>
2022-03-02 16:14:32 -0300

    Ajuste final

--- MataProcessos/src/view/Main.java
+++ MataProcessos/src/view/Main.java
@@ -1,21 +1,52 @@
 package view;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import controller.KillController;
 
 public class Main {
 	
 	static KillController killController = new KillController();
+	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		try {
-			killController.killByName("/usr/bin/terminator");
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-
+		System.out.println("Bem vindo ao MATADOR DE PROCESSOS!");
+		int op;
+		String param;
+		do {
+			System.out.println("\nDigite:"
+					+ "\n0 Para sair."
+					+ "\n1 Para ver a lista de processos."
+					+ "\n2 Para matar processo pelo PID. "
+					+ "\n3 Para matar processo pelo nome. \n");
+			op = input.nextInt();
+			try {
+				switch (op) {
+					case 1:
+						killController.taskList();
+						break;
+					case 2:
+						System.out.println("Digite o PID da task que deseja matar.");
+						param = input.next();
+						killController.killByPid(param);
+						break;
+					case 3:
+						System.out.println("Digite o nome da task que deseja matar.");
+						param = input.next();
+						killController.killByName(param);
+						break;
+					case 0:
+						System.out.println("Obrigado por utilizar este programa.");
+						break;
+					default:
+						System.out.println("Opção desconhecida. Escolha novamente");
+						break;
+				}
+			}catch (IOException e) {
+				System.err.println("Erro. não foi possível executar o processo. Mensagem:" + e.getMessage() );
+			}
+		}while(op!=0);
 	}
 
 }
